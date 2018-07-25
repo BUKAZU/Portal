b70d37c4c920cf9ce2c0778a35e4dde35fd7f457
Bob van Oorschot <bobvanoorschot@gmail.com>
2018-07-25 15:05:22 +0200

    Check object-code

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,6 @@ class App extends Component {
   render() {
       const id = this.props.portalCode
       const objectId = this.props.objectCode
-      console.log(objectId);
 
     return (
         <Query query={PORTAL_QUERY} variables={{ id }}>
@@ -53,7 +52,7 @@ class App extends Component {
 
               const PortalSite = data.PortalSite
 
-              if (objectId !== '' || objectId !== null) {
+              if (objectId !== null) {
                   return <CalendarPage PortalSite={PortalSite} objectId={objectId} />
               } else {
                   return <SearchPage PortalSite={PortalSite} />
