cbe7b01fa01a08d959e19ff804b119c634353d09
Bob van Oorschot <bobvanoorschot@gmail.com>
2018-07-27 17:21:11 +0200

    Started with the calendar

---
+++ src/components/CalendarPage/Calendar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class Calendar extends Component {
+    render() {
+        let months = 4;
+
+        return <div>Calendar</div>;
+    }
+}
+
+export default Calendar;
--- src/components/CalendarPage/CalendarPage.js
+++ src/components/CalendarPage/CalendarPage.js
@@ -1,21 +1,46 @@
 import React, { Component } from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import Calendar from "./Calendar";
 
+export const HOUSE_QUERY = gql`
+    query PortalSiteHousesQuery($id: ID!, $house_id: ID!){
+        PortalSite(id: $id) {
+            houses(
+              id: $house_id
+            ) {
+                id
+                name
+            }
+        }
+    }
+`
 
-
-
-class SearchPage extends Component {
+class CalendarPage extends Component {
   render() {
-    return (
-        <div
-            style={{
-                display: 'flex',
-                flexDirection: 'row'
-            }}
-            >
-            Kalender Pagina
-        </div>
-    )
+      const variables = {
+          id: this.props.PortalSite.portal_code,
+          house_id: '404'
+      }
+
+      return <Query query={HOUSE_QUERY} variables={variables}>
+          {({ loading, error, data }) => {
+            if (loading) return <div>Fetching</div>;
+            if (error) return <div>Error</div>;
+
+            const Results = data.PortalSite.houses;
+
+            return <div id="calendar-container">
+                {Results.map(result => (
+                    <div>
+                        <div>{result.name}</div>
+                        <Calendar />
+                    </div>
+                ))}
+              </div>;
+          }}
+        </Query>;
   }
 }
 
-export default SearchPage;
+export default CalendarPage;
