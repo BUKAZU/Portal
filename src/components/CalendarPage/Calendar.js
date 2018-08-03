2ee957482f084ade8e49f2f9f1f40dc79f27cd89
Bob van Oorschot <bobvanoorschot@gmail.com>
2018-08-03 09:59:02 +0200

    Updated portal calendar

--- src/components/CalendarPage/Calendar.js
+++ src/components/CalendarPage/Calendar.js
@@ -4,6 +4,8 @@ import React, { Component } from 'react';
 class Calendar extends Component {
     render() {
         let months = 4;
+        let currentTime = new Date();
+        let today = currentTime.getDate();
 
         return <div>Calendar</div>;
     }
