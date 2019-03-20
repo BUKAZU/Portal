e2a23c84e8d5e772d33bdb13039441e495f8fb6e
bobvanoorschot <bobvanoorschot@gmail.com>
2019-03-20 10:04:20 +0100

    Use polyfill to fix ms issues

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import '@babel/polyfill';
+
 import Portal from 'bukazu-portal-react';
 import 'bukazu-portal-react/build/index.css';
 
