635077ad5a76cd5695d103a7d13d64ca45de0e6b
Bob van Oorschot <bobvanoorschot@gmail.com>
2020-11-07 17:19:03 +0100

    Modified build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const autoprefixer = require('autoprefixer');
+// const autoprefixer = require('autoprefixer');
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -8,7 +8,7 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
 const InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-const eslintFormatter = require('react-dev-utils/eslintFormatter');
+// const eslintFormatter = require('react-dev-utils/eslintFormatter');
 const ModuleScopePlugin = require('react-dev-utils/ModuleScopePlugin');
 const paths = require('./paths');
 const getClientEnvironment = require('./env');
@@ -112,20 +112,20 @@ module.exports = {
 
       // First, run the linter.
       // It's important to do this before Babel processes the JS.
-      {
-        test: /\.(js|jsx|mjs)$/,
-        enforce: 'pre',
-        use: [
-          {
-            options: {
-              formatter: eslintFormatter,
-              eslintPath: require.resolve('eslint'),
-            },
-            loader: require.resolve('eslint-loader'),
-          },
-        ],
-        include: paths.appSrc,
-      },
+      // {
+      //   test: /\.(js|jsx|mjs)$/,
+      //   enforce: 'pre',
+      //   use: [
+      //     {
+      //       options: {
+      //         formatter: eslintFormatter,
+      //         eslintPath: require.resolve('eslint'),
+      //       },
+      //       loader: require.resolve('eslint-loader'),
+      //     },
+      //   ],
+      //   include: paths.appSrc,
+      // },
       {
         // "oneOf" will traverse all following loaders until one will
         // match the requirements. When no loader matches it will fall
@@ -182,26 +182,26 @@ module.exports = {
                         sourceMap: shouldUseSourceMap,
                       },
                     },
-                    {
-                      loader: require.resolve('postcss-loader'),
-                      options: {
-                        // Necessary for external CSS imports to work
-                        // https://github.com/facebookincubator/create-react-app/issues/2677
-                        ident: 'postcss',
-                        plugins: () => [
-                          require('postcss-flexbugs-fixes'),
-                          autoprefixer({
-                            browsers: [
-                              '>1%',
-                              'last 4 versions',
-                              'Firefox ESR',
-                              'not ie < 9', // React doesn't support IE8 anyway
-                            ],
-                            flexbox: 'no-2009',
-                          }),
-                        ],
-                      },
-                    },
+                    // {
+                    //   loader: require.resolve('postcss-loader'),
+                    //   options: {
+                    //     // Necessary for external CSS imports to work
+                    //     // https://github.com/facebookincubator/create-react-app/issues/2677
+                    //     ident: 'postcss',
+                    //     plugins: () => [
+                    //       require('postcss-flexbugs-fixes'),
+                    //       autoprefixer({
+                    //         browsers: [
+                    //           '>1%',
+                    //           'last 4 versions',
+                    //           'Firefox ESR',
+                    //           'not ie < 9', // React doesn't support IE8 anyway
+                    //         ],
+                    //         flexbox: 'no-2009',
+                    //       }),
+                    //     ],
+                    //   },
+                    // },
                   ],
                 },
                 extractTextPluginOptions
--- src/index.js
+++ src/index.js
@@ -47,7 +47,12 @@ function runTheApp() {
 }
 
 function runApp(element) {
-  const portalCode = element.getAttribute('portal-code');
+  const portalCode = element.getAttribute('portal-code');u-backend:~/sites/reactPortal$ npm run build
+
+> bukazu.react-portal@1.0.1 build /home/bob/sites/reactPortal
+> npm-run-all build-js
+
+
   const objectCode = element.getAttribute('object-code');
   const pageType = element.getAttribute('page');
   const locale = element.getAttribute('language');
